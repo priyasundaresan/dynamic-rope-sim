487a25b9325bc94dceb46af6b9482d386a6f4bb9
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2020-03-15 01:46:47 -0700

    hardcoded knot trajectory worksgit add *

--- rigidbody-rope.py
+++ rigidbody-rope.py
@@ -12,7 +12,7 @@ import random
 from random import sample
 import bmesh
 
-'''Usage: blender -b -P cloth-blender.py'''
+'''Usage: blender -P rigidbody-rope.py'''
 
 def clear_scene():
     '''Clear existing objects in scene'''
@@ -41,6 +41,7 @@ def make_rope(params):
     cylinder.rotation_euler = (0, np.pi/2, 0)
     bpy.ops.rigidbody.object_add()
     cylinder.rigid_body.mass = params["segment_mass"]
+    cylinder.rigid_body.friction = params["segment_friction"]
     #cylinder.rigid_body.kinematic = True 
     for i in range(num_segments-1):
         bpy.ops.object.duplicate_move(TRANSFORM_OT_translate={"value":(-2*segment_radius, 0, 0)})
@@ -55,10 +56,53 @@ def make_table(params):
     table.rigid_body.type = 'PASSIVE'
     bpy.ops.object.select_all(action='DESELECT')
 
+def action_test(params):
+    # Makes a knot by a hardcoded trajectory
+    # Press Spacebar once the Blender script loads to run the animation
+
+    end1 = bpy.data.objects['Cylinder']
+    end2 = bpy.data.objects['Cylinder.%03d'%(params["num_segments"]-1)]
+
+    # Allow endpoints to be keyframe-animated
+    end1.rigid_body.enabled = False
+    end1.rigid_body.kinematic = True
+    end2.rigid_body.enabled = False
+    end2.rigid_body.kinematic = True
+
+    end1.keyframe_insert(data_path="location", frame=1)
+    end2.keyframe_insert(data_path="location", frame=1)
+
+    end2.location[0] += 10
+    end1.location[0] -= 15
+    end1.location[1] += 5 
+    end1.keyframe_insert(data_path="location", frame=80)
+    end2.keyframe_insert(data_path="location", frame=80)
+
+    end1.location[0] -= 1
+    end1.location[1] -= 7
+    end1.keyframe_insert(data_path="location", frame=120)
+
+    end1.location[0] += 3
+    end1.location[2] -= 4
+    end1.keyframe_insert(data_path="location", frame=150)
+
+    end1.location[1] += 3
+    end1.keyframe_insert(data_path="location", frame=170)
+
+    end1.location[2] -= 2
+    end1.keyframe_insert(data_path="location", frame=180)
+
+    end1.location[0] += 5
+    end1.location[2] += 2
+    end1.keyframe_insert(data_path="location", frame=200)
+
+    end1.location[0] += 5
+    end1.keyframe_insert(data_path="location", frame=230)
+
 if __name__ == '__main__':
     with open("rigidbody_params.json", "r") as f:
         params = json.load(f)
     clear_scene()
     make_rope(params)
     make_table(params)
-    
+    action_test(params)
