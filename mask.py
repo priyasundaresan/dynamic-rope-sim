45c87bbc7be4523e6fabfa4a1e27bbf0f413b278
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2020-04-10 13:28:48 -0700

    Code for making videos, generating non-visible masks in descriptor training

---
+++ mask.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import os
+import argparse
+
+def mask(image_filename, directory, texture):
+    ''' Produces a mask of a depth image by thresholding '''
+    img = cv2.imread('./%s/%s'%(directory, image_filename)).copy()
+    mask = img/255.0
+    mask_filename = image_filename.replace('visible_mask', 'mask')
+    cv2.imwrite('image_masks/{}'.format(mask_filename), mask)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir', type=str, default='image_masks')
+    parser.add_argument('--texture', action='store_true')
+    args = parser.parse_args()
+    for filename in os.listdir('./{}'.format(args.dir)):
+    	try:
+    		print("Masking %s" % filename)
+    		mask(filename, args.dir, args.texture)
+    	except:
+            pass
