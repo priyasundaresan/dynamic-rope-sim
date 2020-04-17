8a63aceda32f35b05022da11f465bc0ff57d61fb
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2020-04-16 23:04:20 -0700

    script to visualize bounding boxes

---
+++ vis_bbox.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import argparse
+import os
+import math
+import json
+import colorsys
+
+def show_knots(idx, knots_info, save=True):
+    image_filename = "{0:05d}.jpg".format(idx)
+    img = cv2.imread('images/{}'.format(image_filename))
+    pixels = knots_info[str(idx)]
+    pixels = [i[0] for i in pixels]
+    vis = img.copy()
+    print("Annotating %06d"%idx)
+    start, end = pixels
+    vis = cv2.rectangle(vis, tuple(start), tuple(end), (255, 0, 0), 2) 
+    #for i, (u, v) in enumerate(pixels):
+    #    (r, g, b) = colorsys.hsv_to_rgb(float(i)/len(pixels), 1.0, 1.0)
+    #    R, G, B = int(255 * r), int(255 * g), int(255 * b)
+    #    cv2.circle(vis,(int(u), int(v)), 1, (R, G, B), -1)
+    if save:
+    	annotated_filename = "{0:06d}_annotated.png".format(idx)
+    	cv2.imwrite('./annotated/{}'.format(annotated_filename), vis)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--num', type=int, default=len(os.listdir('./images')) - 1)
+    args = parser.parse_args()
+    if not os.path.exists("./annotated"):
+        os.makedirs('./annotated')
+    else:
+        os.system("rm -rf ./annotated")
+        os.makedirs("./annotated")
+    print("parsed")
+    with open("images/knots_info.json", "r") as stream:
+    	knots_info = json.load(stream)
+    print("loaded knots info")
+    for i in range(args.num):
+        show_knots(i, knots_info)
