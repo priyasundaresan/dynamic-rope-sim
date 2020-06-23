2467f373f236504083e933cff7295ccaa536282c
Jennifer Grannen <jenngrannen@berkeley.edu>
2020-06-22 21:33:44 -0700

    oracle policy on docker cpu worksgit status!

--- docker/setup_environment.sh
+++ docker/setup_environment.sh
@@ -1,7 +1,9 @@
-export DC_SOURCE_DIR=~/code
+export DC_SOURCE_DIR=/code/dense_correspondence
+export MRCNN_SOURCE_DIR=/code/mrcnn_bbox/tools
 
-export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR/modules
-export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR
+export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR/pytorch-segmentation-detection
+export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR/tools
+export PYTHONPATH=$PYTHONPATH:$MRCNN_SOURCE_DIR
 export PATH=$PATH:$DC_SOURCE_DIR/bin
 export PATH=$PATH:$DC_SOURCE_DIR/modules/dense_correspondence_manipulation/scripts
 use_director(){
