c9b06879ab58c05b21d7f349943afca4e9a89ef4
Jennifer Grannen <jgrannen@gmail.com>
2020-07-27 02:16:58 -0700

    multihead (2) implemented

--- policies/multi_head.py
+++ policies/multi_head.py
@@ -11,100 +11,97 @@ import tensorflow as tf
 # BASE_DIR = '/Users/priyasundaresan/Desktop/blender/dynamic-rope'
 BASE_DIR = '/Users/jennifergrannen/Documents/Berkeley/projects/rope/dynamic-rope-sim'
 sys.path.append(BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, "keypoints_cls"))
+sys.path.insert(0, os.path.join(BASE_DIR, "keypoints_cls/src"))
 
 from untangle_utils import *
 from render import find_knot
-from dataset import transform
 from torchvision import transforms
 from keras.models import Model, load_model
+from src.model_multi_headed import KeypointsGauss
+from src.dataset import KeypointsDataset, transform
+from src.prediction import Prediction
+from datetime import datetime
+from PIL import Image
+import numpy as np
 
-def load_nets(path_to_refs, network_dir):
+def load_nets(path_to_refs, network_dir, use_cuda=0):
     with open('%s/ref.json'%(path_to_refs), 'r') as f:
         ref_annots = json.load(f)
-    # return load_model(os.path.join(network_dir, ref_annots["multi_head"], "saved-model-20-380.131.hdf5"), {'tf': tf})
-    return load_model(os.path.join(network_dir, ref_annots["multi_head"], "two_head_untangling.hdf5"), {'tf': tf})
-
-def img_reshape(input_img):
-    img_dim = [640,480,3]
-    _img = np.transpose(input_img, (1, 2, 0))
-    _img = np.flipud(_img)
-    _img = np.reshape(_img, (1, img_dim[0], img_dim[1], img_dim[2]))
-    return _img
-
-def plot_pred_actions(img, act_pred):
-    pull_loc, drop_loc, hold_loc = act_pred.astype(int)
-    cv2.circle(img, tuple(hold_loc), 3, (255,0,0), -1)
-    cv2.arrowedLine(img, tuple(pull_loc), tuple(drop_loc), (0,255,0), 2)
-    return img
-
-def plot_predictions(img, pred):
-    vis = img.copy()
-    #(keypoints, reid, terminate) = pred
-    (keypoints, terminate) = pred
-    keypoints = keypoints.reshape((4,2)).astype(int)
-    #reid = reid[0].argmax()
-    terminate = terminate[0].argmax()
-#    if reid:
-#        cv2.putText(vis, "Take Reid", (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
-#	0.7, (255, 255, 255), 2)
-    if terminate:
-        cv2.putText(vis, "Undone", (10, 55), cv2.FONT_HERSHEY_SIMPLEX,
-	0.7, (255, 255, 255), 2)
-    for i, (u, v) in enumerate(keypoints):
-        (r, g, b) = colorsys.hsv_to_rgb(float(i)/len(keypoints), 1.0, 1.0)
-        R, G, B = int(255 * r), int(255 * g), int(255 * b)
-        cv2.circle(vis,(int(u), int(v)), 4, (R, G, B), -1)
-    return vis
-
-def preds(model, path_to_curr_img, curr_frame):
-    img = cv2.imread(path_to_curr_img)
-    input_img = img_reshape(img)
-    pred = model.predict(input_img)
-    vis = plot_predictions(img, pred)
-    if '%05d.jpg'%curr_frame not in os.listdir('preds'):
-        cv2.imwrite('preds/%05d.jpg'%curr_frame, vis)
+    keypoints = KeypointsGauss(4, img_height=480, img_width=640)
+    model_path = os.path.join(network_dir, "checkpoints", ref_annots["multi_head"])
+    if use_cuda:
+        torch.cuda.set_device(0)
+        keypoints.load_state_dict(torch.load(os.path.join(model_path, os.listdir(model_path)[0])))
+        keypoints = keypoints.cuda()
     else:
-        cv2.imwrite('preds/%05d_2.jpg'%curr_frame, vis)
-    return pred
+        keypoints.load_state_dict(torch.load(os.path.join(model_path, os.listdir(model_path)[0]), map_location='cpu'))
+    return keypoints
+
+def preds(model, path_to_curr_img, curr_frame, use_cuda=0):
+    prediction = Prediction(model, 4, 480, 640, use_cuda)
+    transform = transforms.Compose([transforms.ToTensor()])
+    img = cv2.imread(path_to_curr_img)
+    img_t = transform(img)
+    if use_cuda:
+        img_t = img_t.cuda()
+    heatmap, cls = prediction.predict(img_t)
+    cls = torch.argmax(cls).item()
+    heatmap = heatmap.detach().cpu().numpy()
+    keypoints = []
+    for i in range(4):
+        h = heatmap[0][i]
+        pred_y, pred_x = np.unravel_index(h.argmax(), h.shape)
+        keypoints.append([pred_x, pred_y])
+    classes = {0: "Undo", 1:"Reidemeister", 2:"Terminate"}
+    prediction.plot(img, heatmap, image_id=curr_frame, cls=cls, classes=classes)
+    print("KEYPONTS", keypoints)
+    return keypoints, cls
 
 class MultiHead(object):
     def __init__(self, path_to_refs, network_dir, params):
-        net = load_nets(path_to_refs, network_dir)
+        self.use_cuda = torch.cuda.is_available()
+        net = load_nets(path_to_refs, network_dir, use_cuda=self.use_cuda)
         self.network = net
         self.action_count = 0
         self.rope_length = params["num_segments"]
-        self.end1_kp_idx = 0
-        self.end2_kp_idx = 3
+        self.end1_kp_idx = 3
+        self.end2_kp_idx = 0
         self.pull_kp_idx = 1
         self.hold_kp_idx = 2
+        self.step = 4
 
     def find_pull_hold(self, start_frame, render_offset=0, depth=0):
-        img_num = start_frame-render_offset
+        img_num = max(floor((start_frame-render_offset)/self.step)-1, 0)
         path_to_curr_img = "images/%06d_rgb.png" % (img_num)
         path_to_curr_img_depth = "images_depth/%06d_rgb.png" % (img_num)
 
-        pred = preds(self.network, path_to_curr_img, start_frame-render_offset)
-        (keypoints, terminate) = pred
-        keypoints = keypoints[0]
-        pull_pixel = keypoints[2*self.pull_kp_idx:2*self.pull_kp_idx+2]
-        hold_pixel = keypoints[2*self.hold_kp_idx:2*self.hold_kp_idx+2]
+        pred = preds(self.network, path_to_curr_img, start_frame-render_offset, use_cuda=self.use_cuda)
+        (keypoints, cls) = pred
+        pull_pixel = keypoints[self.pull_kp_idx]
+        hold_pixel = keypoints[self.hold_kp_idx]
+        print("PULL PIXEL", pull_pixel)
+        print("HOLD PIXEL", hold_pixel)
 
         pull_pixel = (int(pull_pixel[0]), int(pull_pixel[1]))
         hold_pixel = (int(hold_pixel[0]), int(hold_pixel[1]))
         return pull_pixel, hold_pixel
 
-    def policy_undone_check(self, start_frame, prev_pull, prev_hold, prev_action_vec, render_offset=0):
+    def reid_check(self, start_frame, render_offset=0):
         # JENN FIX THIS
-        pred = preds(self.network, path_to_curr_img, start_frame-render_offset)
-        (keypoints, terminate) = pred
-        return terminate
+        img_num = max(floor((start_frame-render_offset)/self.step)-1, 0)
+        path_to_curr_img = "images/%06d_rgb.png" % (img_num)
+        pred = preds(self.network, path_to_curr_img, start_frame-render_offset, use_cuda=self.use_cuda)
+        (keypoints, cls) = pred
+        return cls == 1
 
-    def bbox_untangle(self, start_frame, render_offset=0):
-        # JENN assume there is at least one knot at first
-        return True, None
-        # pred = preds(self.bc_network, path_to_curr_img, start_frame-render_offset)
-        # (keypoints, terminate) = pred
-        # return None if terminate else True, None
+    def terminate_check(self, start_frame, render_offset=0):
+        img_num = max(floor((start_frame-render_offset)/self.step)-1, 0)
+        print("IMG NUM", img_num)
+        path_to_curr_img = "images/%06d_rgb.png" % (img_num)
+        pred = preds(self.network, path_to_curr_img, start_frame-render_offset, use_cuda=self.use_cuda)
+        (keypoints, cls) = pred
+        return cls == 2
 
     def undo(self, start_frame, render=False, render_offset=0):
         pull_pixel, hold_pixel = self.find_pull_hold(start_frame, render_offset=render_offset)
@@ -118,7 +115,8 @@ class MultiHead(object):
 
         print("hold", hold_pixel)
         print("pull", pull_pixel)
-        path_to_curr_img = "images/%06d_rgb.png" % (start_frame-render_offset)
+        img_num = max(floor((start_frame-render_offset)/self.step)-1,0)
+        path_to_curr_img = "images/%06d_rgb.png" % (img_num)
         img = cv2.imread(path_to_curr_img)
         img = cv2.circle(img, tuple(hold_pixel), 5, (255, 0, 0), 2)
         img = cv2.circle(img, tuple(pull_pixel), 5, (0, 0, 255), 2)
@@ -132,26 +130,27 @@ class MultiHead(object):
         end_frame, action_vec = take_undo_action(start_frame, pull_idx, hold_idx, action_vec, render=render, render_offset=render_offset)
 
         self.action_count += 1
-        return end_frame+50, pull_pixel, hold_pixel, action_vec
+        return end_frame
 
     def reidemeister(self, start_frame, render=False, render_offset=0):
-        path_to_curr_img = "images/%06d_rgb.png"%(start_frame-render_offset)
-        pred = preds(self.network, path_to_curr_img, start_frame-render_offset)
-        (keypoints, terminate) = pred
-        keypoints = keypoints[0]
-        end1_pixel = keypoints[2*self.end1_kp_idx:2*self.end1_kp_idx+2]
-        end2_pixel = keypoints[2*self.end2_kp_idx:2*self.end2_kp_idx+2]
+        # end1 is the right endpoint
+        img_num = max(floor((start_frame-render_offset)/self.step)-1, 0)
+        path_to_curr_img = "images/%06d_rgb.png" % (img_num)
+        pred = preds(self.network, path_to_curr_img, start_frame-render_offset, use_cuda=self.use_cuda)
+        (keypoints, cls) = pred
+        end1_pixel = keypoints[self.end1_kp_idx]
+        end2_pixel = keypoints[self.end2_kp_idx]
 
         end2_idx = pixels_to_cylinders([end2_pixel])
         end1_idx = pixels_to_cylinders([end1_pixel])
         middle_frame = reidemeister_right(start_frame, end1_idx, end2_idx, render=render, render_offset=render_offset)
 
-        path_to_curr_img = "images/%06d_rgb.png"%(middle_frame-1-render_offset)
-        pred = preds(self.network, path_to_curr_img, start_frame-render_offset)
-        (keypoints, terminate) = pred
-        keypoints = keypoints[0]
-        end1_pixel = keypoints[2*self.end1_kp_idx:2*self.end1_kp_idx+2]
-        end2_pixel = keypoints[2*self.end2_kp_idx:2*self.end2_kp_idx+2]
+        img_num = floor((middle_frame-render_offset)/self.step) -1
+        path_to_curr_img = "images/%06d_rgb.png" % (img_num)
+        pred = preds(self.network, path_to_curr_img, start_frame-render_offset, use_cuda=self.use_cuda)
+        (keypoints, cls) = pred
+        end1_pixel = keypoints[self.end1_kp_idx]
+        end2_pixel = keypoints[self.end2_kp_idx]
 
         end2_idx = pixels_to_cylinders([end2_pixel])
         end1_idx = pixels_to_cylinders([end1_pixel])
@@ -161,7 +160,7 @@ class MultiHead(object):
 if __name__ == '__main__':
     # BASE_DIR = '/Users/priyasundaresan/Desktop/blender/dynamic-rope'
     BASE_DIR = '/Users/jennifergrannen/Documents/Berkeley/projects/rope/dynamic-rope-sim'
-    KP_DIR = os.path.join(BASE_DIR, 'multi_head')
+    KP_DIR = os.path.join(BASE_DIR, 'keypoints_cls')
     path_to_refs = os.path.join(BASE_DIR, 'references', 'capsule')
     with open("../rigidbody_params.json", "r") as f:
         params = json.load(f)
